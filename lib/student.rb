3dc7cd0faaaf17c232457466dca32802335011f4
jared frankel <jaredfrankel10@gmail.com>
2018-01-29 21:46:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,12 @@
 require_relative 'user'
  class Student < User
-   attr_reader :knowledge  
+   attr_reader :knowledge
    def initialize
     @knowledge = []
    end
- 
+
    def learn(subject)
      @knowledge << subject
    end
- 
+
  end
